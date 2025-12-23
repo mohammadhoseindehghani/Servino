d76c891f6a9f640c44ba2026192b7e0a39b712bf
Mohammadhosein Dehghani <dehghani.fullstack@gmail.com>
2025-12-23 11:23:42 +0330

    Setup project skeleton and folder structure

---
+++ src/Presentation/Servino.Presentation.RazorPagesUI/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Servino.Presentation.RazorPagesUI.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
