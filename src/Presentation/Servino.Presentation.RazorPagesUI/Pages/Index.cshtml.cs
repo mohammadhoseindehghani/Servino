d45556c0529893137321d527108908ce4dad52af
Mohammadhosein Dehghani <dehghani.fullstack@gmail.com>
2025-12-29 15:36:26 +0330

    update index ui based on website need

--- src/Presentation/Servino.Presentation.RazorPagesUI/Pages/Index.cshtml.cs
+++ src/Presentation/Servino.Presentation.RazorPagesUI/Pages/Index.cshtml.cs
@@ -1,12 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Servino.Domain.Core.CategoryAgg.Entity;
+using Servino.Infa.Db.SqlServer.EfCore.DbContexts;
 
 namespace Servino.Presentation.RazorPagesUI.Pages
 {
-    public class IndexModel : PageModel
+    public class IndexModel(AppDbContext context) : PageModel
     {
+        //for checking ui
+        public List<Category> Categories { get; set; }
         public void OnGet()
         {
+            Categories = context.Categories.ToList();
         }
     }
 }
